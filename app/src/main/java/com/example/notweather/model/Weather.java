368833dca377ea95bf2ca9529f4cf421abe7bb08
Isaac <isaac.payne.nz@gmail.com>
2019-06-05 22:45:59 +1200

    Update Cards UI

--- app/src/main/java/com/example/notweather/model/Weather.java
+++ app/src/main/java/com/example/notweather/model/Weather.java
@@ -1,10 +1,19 @@
 package com.example.notweather.model;
 
 import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Ignore;
+import android.graphics.Bitmap;
+import android.util.Log;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import com.squareup.picasso.Picasso;
+import java.io.IOException;
 
 public class Weather {
+    @Ignore private final String TAG = Weather.class.getSimpleName();
+    @Ignore private final int TARGET_WIDTH_HEIGHT = 75;
+    @Ignore private final String RAIN = "Rain";
+    @Ignore private final String NOT_RAIN = "Not Rain";
 
     @ColumnInfo(name = "weather_id")
     @SerializedName("id")
@@ -37,6 +46,14 @@ public class Weather {
         return id;
     }
 
+    public String getRainNotRain() {
+        if (RAIN.equals(main)) {
+            return RAIN;
+        }
+
+        return NOT_RAIN;
+    }
+
     public String getMain() {
         return main;
     }
@@ -47,5 +64,24 @@ public class Weather {
 
     public String getIcon() {
         return icon;
+    }
+
+    /**
+     * Not really sure if this should live here, but Picasso seems to break the MVVM pattern
+     *
+     * @return The Icon Bitmap to be displayed in an ImageView
+     */
+    public Bitmap getIconBitmap() {
+        String url = createIconUrl();
+        try {
+            return Picasso.get().load(url).resize(TARGET_WIDTH_HEIGHT, TARGET_WIDTH_HEIGHT).get();
+        } catch (IOException e) {
+            Log.w(TAG, String.format("Failed to load img: %s", url));
+            return null;
+        }
+    }
+
+    private String createIconUrl() {
+        return String.format("https://api.openweathermap.org/img/w/%s.png", icon);
     }
 }
